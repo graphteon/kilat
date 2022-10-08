46fe2feb5cf6faff9ae1574a2ebee76c27516ce8
ibnu yahya <anak10thn@gmail.com>
2022-10-08 21:52:12 +0700

    Add test

--- examples/graphql.ts
+++ examples/graphql.ts
@@ -1,6 +1,8 @@
 import { Server } from "https://deno.land/x/kilat@1.1.0/mod.ts";
 import { makeExecutableSchema, GraphQLHTTP, gql } from 'https://deno.land/x/kilat@1.1.0/middlewares/graphql/mod.ts'
 
+// TEST curl -X POST localhost:3000/graphql -d '{ "query": "{ hello }" }'
+
 const port = 3000;
 const typeDefs = gql`
   type Query {
